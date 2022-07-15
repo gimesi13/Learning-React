87fbaa3f438aaf4c3231ff1d90029927c64656e5
Gergely Gimesi <105725861+gimesi13@users.noreply.github.com>
2022-07-15 08:40:59 +0300

    version 7/15/2022

--- react-project1/src/components/App.js
+++ react-project1/src/components/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import CountButton from './CountButton'
+import React from "react";
+import CountButton from "./CountButton/CountButton";
 
 const App = () => {
-  return(
+  return (
     <div>
-        <CountButton incrementBy={1} buttonColor={'blue'} />
-        <CountButton incrementBy={5} buttonColor={'green'} />
-        <CountButton incrementBy={10} buttonColor={'red'} />
+      <CountButton incrementBy={1} buttonColor={"blue"} />
+      <CountButton incrementBy={5} buttonColor={"green"} />
+      <CountButton incrementBy={10} buttonColor={"#ff0000"} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- react-project1/src/index.js
+++ react-project1/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./styles/global.css";
 
-const reactContentRoot = document.getElementById('root')
+const reactContentRoot = document.getElementById("root");
 
-ReactDOM.render(<App />, reactContentRoot)
+ReactDOM.render(<App />, reactContentRoot);
