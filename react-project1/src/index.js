e514e4c8d1c2084c07fabc611eec63a621b99102
gimesi13 <105725861+gimesi13@users.noreply.github.com>
2022-08-03 17:25:51 +0200

    cleaning up

--- react-project1/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./styles/global.css";
-
-const reactContentRoot = document.getElementById("root");
-
-ReactDOM.render(<App />, reactContentRoot);
